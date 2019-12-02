1a6abb0008183c69284798797201a559f8da7190
Dace De vALDENA <dace_bobovica@inbox.lv>
2019-12-02 19:49:31 +0200

    ..

--- MY_PROJECT/src/com/accentureFinalProject/AnimalsApp.java
+++ MY_PROJECT/src/com/accentureFinalProject/AnimalsApp.java
@@ -5,6 +5,7 @@ public class AnimalsApp {
 	public static void main(String[] args) {
 		Animals monkey = new Animals("Monkey", "Tree", Gender.male, 50);
 		Animals giraffe = new Animals("Giraffe", "Savanna", Gender.female, 70);
+		Animals tiger = new Animals ("Tiger", "Rainy forest", Gender.male, 90);
 		monkey.setFoodAmount(20);
 		System.out.println(monkey.getFoodAmount());
 		monkey.increaseFoodAmount(20);

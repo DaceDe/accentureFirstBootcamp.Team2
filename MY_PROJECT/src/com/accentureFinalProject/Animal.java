078e326ebde1c5b542b42e9195d6858f0cdbe692
haveamango <dagolovinad@gmail.com>
2019-12-05 23:58:30 +0200

    final

---
+++ MY_PROJECT/src/com/accentureFinalProject/Animal.java
@@ -0,0 +1,180 @@
+package com.accentureFinalProject;
+
+import java.util.Scanner;
+
+public class Animal {
+	private String name;
+	private String habitat;
+	private boolean hungry;
+	private int foodAmount;
+	private boolean gender;
+	private HappinessLevel happynessLevel;
+	private boolean foodPreferences;
+	static Scanner scanner = new Scanner(System.in);
+
+	public Animal(String name, String habitat, int foodAmount, boolean gender, boolean foodPreferences,
+			double foodConsumed, int happynessLevel) {
+		this.name = name;
+		this.habitat = habitat;
+		this.gender = gender;
+		this.foodPreferences = foodPreferences;
+		this.foodAmount = foodAmount;
+		this.happynessLevel = new HappinessLevel();
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getHabitat() {
+		return habitat;
+	}
+
+	public double getFoodAmount() {
+		return foodAmount;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+
+	public void setHungry(boolean hungry) {
+		this.hungry = hungry;
+	}
+
+	public void setFoodAmount(int foodAmount) {
+		this.foodAmount = foodAmount;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public boolean isFoodPreferences() {
+		return foodPreferences;
+	}
+
+	public void setFoodPreferences(boolean foodPreferences) {
+		this.foodPreferences = foodPreferences;
+	}
+
+	public void shareFood(Animal anotherAnimal) {
+		if (this == anotherAnimal) {
+			System.out.println("You can`t share food with yourself!");
+		} else {
+			int toShare = this.foodAmount / 2;
+			anotherAnimal.foodAmount += toShare;
+			this.foodAmount -= toShare;
+			System.out.println(name + " has shared " + toShare + " with " + anotherAnimal.name);
+		}
+	}
+
+	public boolean makeABaby(Animal anotherAnimal) {
+		if (this == anotherAnimal) {
+			System.out.println("You can`t make a baby alone!");
+			return false;
+		}
+		if (this.happynessLevel.isMax() && anotherAnimal.happynessLevel.isMax()
+				&& isCompatibleForLove(anotherAnimal) == true) {
+			System.out.println("Congratulations! Your animals have a baby!");
+			return true;
+		} else {
+			System.out.println("Your animals can't have a baby!");
+			return false;
+		}
+
+	}
+
+	private boolean isCompatibleForLove(Animal anotherAnimal) {
+		if (this.gender == Gender.female && anotherAnimal.gender == Gender.male
+				|| this.gender == Gender.male && anotherAnimal.gender == Gender.female) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void stealFood(Animal anotherAnimal) {
+		if (this == anotherAnimal) {
+			System.out.println("You can`t steal food from yourself!");
+		} else {
+			if (anotherAnimal.foodAmount == 0) {
+				System.out.println(name + " hasn't stole anything since " + anotherAnimal.name + " had no food");
+				return;
+			}
+			this.foodAmount += anotherAnimal.foodAmount;
+			anotherAnimal.foodAmount = 0;
+			System.out.println(name + " has stolen all the food from " + anotherAnimal.name + " and now has "
+					+ this.foodAmount + " food");
+		}
+	}
+
+	public void eat(String food) {
+		if (food == null) {
+			System.out.println(
+					"Sorry, " + name + " doesn't eat that and now is " + happynessLevel.decrease() + " level happy.");
+		} else {
+			switch (food) {
+			case "ball":
+				System.out.println("Sorry, " + name + " doesn't eat ball and now is " + happynessLevel.decrease()
+						+ " level happy");
+				break;
+			case "meat":
+				if (foodPreferences == FoodPreferences.predator) {
+					System.out.println("You have fed the " + name + " with meat and now is " + happynessLevel.increase()
+							+ " level happy.");
+				} else {
+					System.out.println("Sorry, " + name + " is herbivore and doesn't eat meat and now is "
+							+ happynessLevel.decrease() + " level happy.");
+				}
+				break;
+			case "grass":
+				if (foodPreferences == FoodPreferences.herbivore) {
+					System.out.println("You have fed the " + name + " with grass and now is "
+							+ happynessLevel.increase() + " level happy.");
+				} else {
+					System.out.println("Sorry, " + name + " is predator and doesn't eat grass and now is  "
+							+ happynessLevel.decrease() + " level happy.");
+				}
+				break;
+			}
+		}
+	}
+
+	public void play(String toy) {
+		if (toy != null) {
+			System.out.println("You have played with " + toy + " and " + name + " now is level "
+					+ happynessLevel.increase() + " happy");
+		} else {
+			System.out.println("You cannot play with that: " + happynessLevel.decrease() + " happy");
+		}
+	}
+
+	public void changeHabitat(String habitat) { // scanner
+		this.habitat = habitat;
+		System.out.println("Animal now lives in " + habitat);
+	}
+
+	public String getGenderString() {
+		return gender == Gender.female ? "FEMALE" : "MALE";
+	}
+
+	public String getfoodPreferencesString() {
+		return foodPreferences == FoodPreferences.herbivore ? "herbivore" : "predator";
+	}
+
+	public String toString() {
+		return name + ". It leaves in " + habitat + ", has to eat " + foodAmount + " killograms of food, "
+				+ " gender is " + getGenderString() + " and it " + getfoodPreferencesString() + "\n";
+	}
+
+}
--- MY_PROJECT/src/com/accentureFinalProject/AnimalsApp.java
+++ MY_PROJECT/src/com/accentureFinalProject/AnimalsApp.java
@@ -1,40 +1,146 @@
 package com.accentureFinalProject;
 
+import java.util.Scanner;
+
 public class AnimalsApp {
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	static Animal monkey = new Animal("Monkey", "Tree", 50, Gender.male, FoodPreferences.herbivore, 0, 0);
+	static Animal giraffe = new Animal("Giraffe", "Savanna", 70, Gender.female, FoodPreferences.herbivore, 0, 0);
+	static Animal tiger = new Animal("Tiger", "Rainy forest", 90, Gender.male, FoodPreferences.predator, 0, 0);
 
 	public static void main(String[] args) {
-		Animals monkey = new Animals("Monkey", "Tree",50, Gender.male, FoodPreferences.herbivore, 0, 0);
-		Animals giraffe = new Animals("Giraffe", "Savanna",70, Gender.female, FoodPreferences.herbivore, 0, 0);
-		Animals tiger = new Animals ("Tiger", "Rainy forest",90, Gender.male, FoodPreferences.predator, 0, 0);
-		//System.out.println(tiger.toString());
 		
-//		monkey.setFoodAmount(20);
-//		System.out.println(monkey.getFoodAmount());
-//		monkey.increaseFoodAmount(20);
-//		System.out.println(monkey.getFoodAmount());
-//		System.out.println(monkey);
-//		System.out.println(giraffe);
-//		
-//		
-//		
-//		
-//		monkey.stealFood(tiger, 15);
-//		monkey.getFoodAmount();
-//		tiger.getFoodAmount();
-//		
-//		tiger.shareFood(giraffe);
-//		tiger.getFoodAmount();
-//		giraffe.getFoodAmount();
-//		
-tiger.startGame();
-//monkey.play();
-//monkey.giveFood();
-//monkey.changeHabitat();		
-		 
-		 }
-		
+
+		System.out.println("Hello, welcome to the calmest game ever!\n"
+				+ "Your goal is to make as much babies as possible and in order to do that, your animals should have level 4 of happyness.\n "
+				+ "You have three animals:\n"
+				+ monkey.toString()+
+				giraffe.toString()+
+				tiger.toString());
+		boolean babyMade = false;
+		while (!babyMade) {
+			babyMade = startGame();
+		}
+	}
+	
+	private static Animal selectAnimal(String message) {
+		System.out.println(
+				message
+				+ "\nEnter: "
+				+ "\n (1) for monkey, "
+				+ "\n (2) for giraffe, "
+				+ "\n (3) for tiger.");
+		String input = scanner.nextLine();// add do while loop
+		switch (input) {
+		case "1":
+			System.out.println("You have chosen monkey");
+			return monkey;
+		case "2":
+			System.out.println("You have chosen giraffe");
+			return giraffe;
+		case "3":
+			System.out.println("You have chosen tiger");
+			return tiger;
+		default:
+			System.out.println("Your entered number is wrong. Try again");
+			return null;
+		}
+	}
+	
+	public static boolean startGame() {
+		Animal animal = selectAnimal("Please choose animal you want to play with:");
+		if (animal == null) {
+			return false;
+		}
+
+		System.out.println(
+				"Please choose what do you want to do with you animal: "
+				+ "\n  Enter: "
+				+ "\n (1) for playing, "
+				+ "\n (2) for giving food, "
+				+ "\n (3) for changing habitat,"
+				+ "\n (4) for stealing food from another animal, "
+				+ "\n (5) for sharing food with another animal "
+				+ "\n (6) for making babies, "
+				+ "\n (7) for choosing another animal. ");
+		String input = scanner.nextLine();
+		switch (input) {
+		case "1":
+			String toy = selectToy();
+			animal.play(toy);
+			return false;
+		case "2":
+			String food = selectFood();
+			animal.eat(food);
+			return false;
+		case "3":
+			String habitat = selectHabitat();
+			animal.changeHabitat(habitat);
+			return false;
+		case "4":
+			Animal target = selectAnimal("Please choose animal to steal from:");
+			animal.stealFood(target);
+			return false;
+		case "5":
+			target = selectAnimal("Please choose animal to share with:");
+			animal.shareFood(target);
+			return false;
+		case "6":
+			target = selectAnimal("Please choose animal to mate with:");
+			return animal.makeABaby(target);			
+		default:
+			System.out.println("Invalid option");
+			return false;
+		}
+	}
 	
 
+	public static String selectToy() {
+		System.out.println(
+				"How do you want to play? "
+				+ "\n Enter: "
+				+ "\n (1) for playing with ball, "
+				+ "\n (2) for playing with frisbee, "
+				+ "\n (3) for playing with stuffed rabbit.");
+		String input = scanner.nextLine();
+		switch (input) {
+		case "1":
+			return "ball";
+		case "2":
+			return "frisbee";
+		case "3":
+			return "stuffed rabbit";
+		default:
+			return null;
+		}
+	}
+	
+	public static String selectHabitat() {
+		System.out.println("Enter the new home for your animal:");
+		return scanner.nextLine();
+	}
+	
+	public static String selectFood() {
+
+		System.out.println(
+				"What food do you want to give to your animal? "
+				+ "\n Enter: "
+				+ "\n (1) ball, "
+				+ "\n (2) meat, "
+				+ "\n (3) grass.");
+		String input = scanner.nextLine();
+		switch (input) {
+		case "1":
+			return "ball";
+		case "2":
+			return "meat";
+		case "3":
+			return "grass";
+		default:
+			return null;
+		}
 	}
 
-
+}
